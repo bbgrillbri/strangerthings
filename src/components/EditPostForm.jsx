2205ed90fc563377cc72b390947da6afe2b30518
GoldenNole <prstokes37@gmail.com>
2023-08-10 20:05:45 -0500

    Fixed edit and cleaned up files

---
+++ src/components/EditPostForm.jsx
@@ -0,0 +1,26 @@
+import { updatePost } from "../API/main";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+const EditPostForm = () => {
+  const navigate = useNavigate();
+  const { postId } = useParams();
+  console.log("postId", postId)
+
+  async function handleUpdate() {
+    try {
+      const result = await updatePost(postId);
+      console.log("Update", result);
+      navigate("/post");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <div>
+      <button onClick={handleUpdate}>Edit Post</button>
+    </div>
+  );
+};
+
+export default EditPostForm;
